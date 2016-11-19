9435c3a3516a69587ccc47c2d78f67943e71f296
renato <renato@knewin.com>
2016-11-18 23:50:26 -0200

    Alterado para monitorar latência via AOP.

--- src/main/java/br/ufsc/grad/renatoback/tcc/post/service/rest/PostController.java
+++ src/main/java/br/ufsc/grad/renatoback/tcc/post/service/rest/PostController.java
@@ -1,7 +1,5 @@
 package br.ufsc.grad.renatoback.tcc.post.service.rest;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,34 +14,10 @@ public class PostController {
 	@Autowired
 	PostService service;
 
-	@RequestMapping(path = "/{time}", method = RequestMethod.POST)
+	@RequestMapping(path = "/{userId}", method = RequestMethod.POST)
 	@ResponseStatus(code = HttpStatus.CREATED)
-	public void send(@PathVariable("time") Long time) {
-		service.sendWelcomePackage(time);
-	}
-
-	@RequestMapping(method = RequestMethod.DELETE)
-	@ResponseStatus(code = HttpStatus.OK)
-	public void resetStatistics() {
-		service.resetStatistics();
-	}
-
-	@ResponseStatus(code = HttpStatus.OK)
-	@RequestMapping(path = "/{threads}/{sleep}", method = RequestMethod.PUT)
-	public void printStatistics(@PathVariable("threads") Integer threads, @PathVariable("sleep") Integer sleep) {
-		service.printStatistics(threads, sleep);
-	}
-
-	@RequestMapping(method = RequestMethod.OPTIONS)
-	@ResponseStatus(code = HttpStatus.OK)
-	public void printTime() {
-		System.out.println("Nano time: " + new Date().getTime());
-	}
-
-	@ResponseStatus(code = HttpStatus.OK)
-	@RequestMapping(path = "/sync/{time}", method = RequestMethod.PUT)
-	public void syncTime(@PathVariable("time") Long time) {
-		service.syncTime(time);
+	public void sendPackage(@PathVariable("userID") Long userId) {
+		service.sendPackage(userId);
 	}
 
 }
--- src/main/java/br/ufsc/grad/renatoback/tcc/post/service/rest/PostService.java
+++ src/main/java/br/ufsc/grad/renatoback/tcc/post/service/rest/PostService.java
@@ -1,43 +1,13 @@
 package br.ufsc.grad.renatoback.tcc.post.service.rest;
 
-import java.util.Date;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostService {
 
-	private static final Long BASE_TIME = 379089900000l;
-
-	private Log logger = LogFactory.getLog(PostService.class);
-
-	private AtomicInteger counter = new AtomicInteger();
-	private AtomicLong average = new AtomicLong();
-	private Long serverDiff;
-
-	public void sendWelcomePackage(Long time) {
-		counter.incrementAndGet();
-		average.accumulateAndGet(new Date().getTime() - time + serverDiff,
-				(n, m) -> (n + m) / (n == 0 || m == 0 ? 1 : 2));
-	}
-
-	public void printStatistics(Integer threads, Integer sleep) {
-		logger.info(String.format("POST [%d threads][%d sleep] - [%d packages][%d average ms]", threads, sleep,
-				counter.get(), average.get()));
-	}
-
-	public void resetStatistics() {
-		counter.set(0);
-		average.set(0);
-	}
-
-	public void syncTime(Long remoteDiff) {
-		long localDiff = System.currentTimeMillis() - BASE_TIME;
-		serverDiff = remoteDiff - localDiff;
+	public void sendPackage(Long userId) {
+		// received request
+		return;
 	}
 
 }
